166d80977e445efd8d2b23e0c97460eececd9f3e
ZhevairovskiBojan <bojanzevairovskijs@gmail.com>
2023-11-10 21:56:01 +0100

    second commit

--- pkg/kursModel/kursSchema.js
+++ pkg/kursModel/kursSchema.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const kursSchema = new mongoose.Schema({
     name: {
         type:String,
-        
+        required: [true, "the name is required"]
     },
     adress: {
         type: String,
-        
+        required: [true, "the address is required"]
     },
     type: {
         type:String,
